f7b40afc56b41a3e24c91c0f44cb9051a74bea68
Bilal Alam <bilal.alam@keeptruckin.com>
2021-06-10 11:32:13 +0500

    Completed basic todo app with components

---
+++ src/todos.svelte
@@ -0,0 +1,182 @@
+<style>
+    .container {
+        max-width: 800px;
+        margin: 10px auto;
+    }
+    .todo-input {
+        width: 100%;
+        padding: 10px, 20px;
+        font-size: 18px;
+        margin-bottom: 20px;
+    }
+    .inner-container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-size: 16px;
+        border-top: 1px solid lightgrey;
+        padding-top: 15px;
+        margin-bottom: 13px;
+    }
+    .inner-container-input {
+        margin-right: 12px;
+    }
+    button {
+        font-size: 14px;
+        background-color: white;
+        appearance: none;
+    }
+    button:hover {
+        background: lightseagreen;
+    }
+    button:focus {
+        outline: none;
+    }
+    .active {
+        background: lightseagreen;
+    }
+</style>
+
+<script>
+  import TodoItem from "./TodoItem.svelte";
+  let newTodoTitle = "";
+  let currentFilter = "all";
+  let nextId = 4;
+  let todos = [
+    {
+      id: 1,
+      title: "My first todo",
+      isComplete: false,
+    },
+    {
+      id: 2,
+      title: "My second todo",
+      isComplete: false,
+    },
+    {
+      id: 3,
+      title: "My third todo",
+      isComplete: false,
+    },
+  ];
+
+  let filteredTodos =
+    currentFilter === "all"
+      ? todos
+      : currentFilter === "completed"
+      ? todos.filter((todo) => todo.completed)
+      : todos.filter((todo) => !todo.completed);
+  let todosRemaining = filteredTodos.filter((todo) => !todo.completed).length;
+
+  function updateState() {
+    filteredTodos =
+    currentFilter === "all"
+      ? todos
+      : currentFilter === "completed"
+      ? todos.filter((todo) => todo.completed)
+      : todos.filter((todo) => !todo.completed);
+  todosRemaining = filteredTodos.filter((todo) => !todo.completed).length;
+  }
+
+  function addTodo(event) {
+    if (event.key === "Enter") {
+      todos = [
+        ...todos,
+        {
+          id: nextId,
+          completed: false,
+          title: newTodoTitle,
+        },
+      ];
+      nextId = nextId + 1;
+      newTodoTitle = "";
+    }
+    updateState();
+  }
+
+  function checkAllTodos(event) {
+    todos.forEach((todo) => (todo.completed = event.target.checked));
+    todos = todos;
+    updateState();
+  }
+
+  function updateFilter(newFilter) {
+    currentFilter = newFilter;
+    updateState();
+  }
+
+  function clearCompleted() {
+    todos = todos.filter((todo) => !todo.completed);
+    updateState();
+  }
+
+  function handleDeleteTodo(event) {
+    todos = todos.filter((todo) => todo.id !== event.detail.id);
+    updateState();
+  }
+
+  function handleToggleComplete(event) {
+    const todoIndex = todos.findIndex((todo) => todo.id === event.detail.id);
+    const updatedTodo = {
+      ...todos[todoIndex],
+      completed: !todos[todoIndex].completed,
+    };
+    todos = [
+      ...todos.slice(0, todoIndex),
+      updatedTodo,
+      ...todos.slice(todoIndex + 1),
+    ];
+    updateState();
+  }
+</script>
+
+<div class="container">
+  <h2>Svelte POC - A Simple Todo App</h2>
+  <input
+    type="text"
+    class="todo-input"
+    placeholder="Insert todo item ..."
+    bind:value={newTodoTitle}
+    on:keydown={addTodo}
+  />
+  {#each filteredTodos as todo}
+    <div class="todo-item">
+      <TodoItem
+        {...todo}
+        on:deleteTodo={handleDeleteTodo}
+        on:toggleComplete={handleToggleComplete}
+      />
+    </div>
+  {/each}
+  <div class="inner-container">
+    <div>
+      <label
+        ><input
+          class="inner-container-input"
+          type="checkbox"
+          on:change={checkAllTodos}
+        />Check All</label
+      >
+    </div>
+    <div>{todosRemaining} items left</div>
+  </div>
+  <div class="inner-container">
+    <div>
+      <button
+        on:click={() => updateFilter("all")}
+        class:active={currentFilter === "all"}>All</button
+      >
+      <button
+        on:click={() => updateFilter("active")}
+        class:active={currentFilter === "active"}>Active</button
+      >
+      <button
+        on:click={() => updateFilter("completed")}
+        class:active={currentFilter === "completed"}>Completed</button
+      >
+    </div>
+    <div>
+      <button on:click={clearCompleted}>Clear Completed</button>
+    </div>
+  </div>
+</div>
